978545587bd93dd935bb83c79c69ffa335badead
赵凯 <zhaokai@angelalign.com>
2023-06-21 16:45:47 +0800

    feat: update

--- helpper/env.ts
+++ helpper/env.ts
@@ -5,7 +5,10 @@ export const DB_URL = process.env.DB_URL;
 export const DB_USER = process.env.DB_USER;
 export const DB_PWD = process.env.DB_PWD;
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+
 export const FILE_STORAGE_ROOT = process.env.FILE_STORAGE_ROOT
 export const FILE_MAX_SIZE = process.env.FILE_MAX_SIZE
+export const FILE_BUCKET_NAME = process.env.FILE_BUCKET_NAME
+
 export const SERVER_PORT = process.env.SERVER_PORT
 export const REDIS_URL = process.env.REDIS_URL
