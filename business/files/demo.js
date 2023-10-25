2c059addc2c636588f236383c5b282062314b37a
z <820641374@qq.com>
2023-10-25 23:37:01 +0800

    feat: update

--- business/files/demo.js
+++
@@ -1,3 +0,0 @@
-import fs from 'fs'
-const file = fs.readFileSync('./index.ts')
-console.log('file: ', file);
--- business/files/fileHandle.ts
+++ business/files/fileHandle.ts
@@ -6,8 +6,23 @@ export const handleMultiPartUpload = ()=>{
 
 }
 
-export const calculateFileMd5 = (filePath:string)=>{
-    const md5 = crypto.createHash('md5');
-    const file = fs.readFileSync(filePath);
-    
+export const calculateFileMd5 = (filePath:string,callback:()=>void)=>{
+    const hash = crypto.createHash('md5');
+    const stat = fs.statSync('./index.ts')
+    if(stat.size  < 1024*1024*100){
+        const buffer = fs.readFileSync(filePath);
+        hash.update(buffer,'utf8');
+        const md5 = hash.digest('hex');
+        return md5;
+    }
+    const fileStream = fs.createReadStream('./index.ts');
+    fileStream.on('data', (chunk:string)=>{
+        hash.update(chunk,'utf8');
+    })
+
+    fileStream.on('end',()=>{
+        const md5 = hash.digest('hex');
+        console.log('md5: ', md5);
+    })
+
 }
